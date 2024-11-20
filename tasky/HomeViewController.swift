8dd0f6d6002e8bf0454c32af4c8a40384a5a6595
felipeassis97 <felipeassis97@gmail.com>
2024-11-20 18:21:36 +1100

    feat: initial commit

---
+++ tasky/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tasky
+//
+//  Created by Felipe Assis on 20/11/2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lilac
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tasky/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  tasky
+//
+//  Created by Felipe Assis on 20/11/2024.
+//
+
+import Foundation
+
+enum Constants {
+    enum Illustration {
+        static let taskIllustration = "task-illustration"
+        static let homeIllustration = "initial-illustration"
+    }
+}
